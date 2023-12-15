9789d321a5e279db34fe8505b26f2bf2439e81b8
Maria Carolina <demycar24@gmail.com>
2023-12-14 21:55:44 -0300

    variaveis PHP

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+//echo "Olá mundo";
+// as variaveis no PHP começam com $ seguido pelo nome da variavel
+$meuNome = "Maria Carolina";
+$idade = 29;
+echo "O nome da programadora é $meuNome e ela tem $idade anos <br>";
+
+//tipos de dados PHP:
+$inteiro = 0;
+$decimal = 10.5;
+$nome = "Débora";
+$verdadeiro = true;
+$lista = array("Iphone 15", "mouse", "headset");
+
+$quantidade_produtos = 62;
+$quantidade_produtos = $quantidade_produtos - 5;
+echo "Temos $quantidade_produtos produtos em nosso estoque <br>";
+
+$saldo = 10000;
+$deposito = 500;
+$saldoTotal = $saldo + $deposito;
+echo "$saldoTotal";
